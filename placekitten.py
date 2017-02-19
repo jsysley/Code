60487aa4236f3808e8fc3882f087a043313e5ca7
Ivan <237149516@qq.com>
2017-02-19 16:55:43 +0800

    2017.2.19

--- placekitten.py
+++ placekitten.py
@@ -9,5 +9,10 @@ import urllib.request
 
 req=urllib.request.Request('http://placekitten.com/')
 response=urllib.request.urlopen(req)
-html=response.read()#因为下面图片要用这个函数，图片decode的话格式不对，因此这里不decde
-print(html)
+#html=response.read().decode('utf-8')#因为下面图片要用这个函数，图片decode的话格式不对，因此这里不decde
+#print(html)
+cat_img=response.read()
+
+#存储图片
+with open ('cat_500_600.jpg','wb') as f:
+    f.write(cat_img)
